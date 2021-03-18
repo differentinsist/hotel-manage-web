6324a3703eca9dacf377054f2d3387949a3cda20
qbj <qbj5201314.yunfd.com>
2021-03-18 16:29:52 +0800

    标记--出错就来到这里，这是没写登陆组件前的状态

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    <h1>登陆组件</h1>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 
-// 懒加载
+// 懒加载(都搞懒加载会不会有问题？？？)
 const Home = () => import('../page/home/Home.vue')
 const Profile = () => import('../page/profile/Profile.vue')
 const HotelMall = () => import('../page/mall/HotelMall.vue')
@@ -11,10 +11,14 @@ const Shop = () => import('../page/mall/Shop')
 const HappyTime = () =>　import('../page/happy/HappyTime.vue')
 const CommentArea = () => import('../page/comment/CommentArea.vue')
 
+const Login = () => import('../page/Login.vue')
+
 Vue.use(Router)
 
 const routes = [
   {path: '/', component: HotelMall},
+  // 意思是当用户访问/login这个路径时跳转到Login这个组件
+  {path: '/login', component: Login},
   {path: '/hotelmall', component: HotelMall},
   // {path: '/myroom', name: '/myroom', component: MyRoom},
   {path: '/myroom',component: MyRoom},
